7dda700d4cfbf9f2879d4cb9d21a33ee74b59804
justo yancan <52587714+jyancan@users.noreply.github.com>
2021-06-24 17:50:32 -0500

    Update ansible.sh

--- ansible/ansible.sh
+++ ansible/ansible.sh
@@ -3,5 +3,5 @@ sudo python -m pip install ansible
 ansible-galaxy init jenkins
 ansible-galaxy collection install google.cloud
 ansible-playbook setup.yml -i ip.txt
-export JENKINSPASSW="$(udo cat /var/lib/jenkins/secrets/initialAdminPassword)"
+export JENKINSPASSW="$(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)"
 
